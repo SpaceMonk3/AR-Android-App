f689c933a48cfa2ba80fc72eb961d47a28bbf1d9
Dheer Tammina <dtammina@gmu.edu>
2023-11-23 22:29:30 -0500

    created an AR fragment and added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.project3_dtammina"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.project3_dtammina"
@@ -35,6 +35,7 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.ar.sceneform.ux:sceneform-ux:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
