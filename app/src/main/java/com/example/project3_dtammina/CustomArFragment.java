21c420fb00371558acf4da9ff59c99d2183bb1ff
Dheer Tammina <dtammina@gmu.edu>
2023-11-24 22:58:27 -0500

    added game loop but sceneform errors, need to update to maintained version of sceneform

--- app/src/main/java/com/example/project3_dtammina/CustomArFragment.java
+++ app/src/main/java/com/example/project3_dtammina/CustomArFragment.java
@@ -8,11 +8,12 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import com.google.ar.sceneform.ux.ArFragment;
-public class ARFragment extends ArFragment {
+public class CustomArFragment extends ArFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         FrameLayout frameLayout = (FrameLayout) super.onCreateView(inflater, container, savedInstanceState);
+
         getPlaneDiscoveryController().hide();
         getPlaneDiscoveryController().setInstructionView(null);
 
